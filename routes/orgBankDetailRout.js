08d9bac78411b1c7e4924ad9e1e13fdfd05e9dc0
unknown <gpittaz67@gmail.com>
2026-07-24 15:58:28 +0100

    jwt 401 return 2

--- routes/orgBankDetailRout.js
+++ routes/orgBankDetailRout.js
@@ -1,9 +1,10 @@
 const {orgBnkDetFXN,orgBnkDetfetchFXN} = require("../controllers/orgBankDetail");
+const {authFxn} = require('../middleware/auth');
 const express = require("express")
 const router = express.Router()
 const { Module } = require("module")
 
 
 router.patch("/orgBankDetails/:userID", orgBnkDetFXN)
-router.get("/orgBankDetailsfetch/:userID", orgBnkDetfetchFXN)
+router.get("/orgBankDetailsfetch/:userID",authFxn, orgBnkDetfetchFXN)
 module.exports = router
